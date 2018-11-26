43351696e4d3f2be09bdb450b40e5a767c404da5
Alexey Gerasimov <leschagerasimov@yandex.ru>
2018-11-26 14:11:25 +0300

    commit

--- test_git/src/Test.java
+++ test_git/src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String []args){
         System.out.println("here");
         System.out.println("brabch1");
+
+        System.out.println("ololo");
     }
 }
