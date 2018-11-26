86c397dc8069fa0ce5213df6d6cd3a2725041202
Alexey Gerasimov <leschagerasimov@yandex.ru>
2018-11-26 16:23:51 +0300

    commit

--- test_git/src/Test.java
+++ test_git/src/Test.java
@@ -9,9 +9,15 @@ public class Test {
         System.out.println("ololo");
         System.out.println("ololo");
         System.out.println("ololo");
-        System.out.println("ololo");
-        System.out.println("ololo");
-        System.out.println("ololo");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
+        System.out.println("ololo2");
         System.out.println("ololo");
     }
 }
