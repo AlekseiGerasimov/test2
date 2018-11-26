e7a6a86d84a4782887fabd282e34573e955ee3c8
Alexey Gerasimov <leschagerasimov@yandex.ru>
2018-11-26 16:27:28 +0300

    fff

--- test_git/src/Test.java
+++ test_git/src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String []args){
         System.out.println("here");
         System.out.println("brabch1");
+        System.out.println("here");
     }
 }
